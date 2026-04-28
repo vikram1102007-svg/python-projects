daa664192ba7c3bab05d6faf71bccfb5dcc1336b
vikram1102007 <vikram1102007@gmail.com>
2026-04-28 23:18:51 +0530

    odd or even file logic

--- oddoreven_finder.py
+++ oddoreven_finder.py
@@ -1,4 +1,5 @@
 n=int(input("enter your number:"))
+sum=0
 if n<0:
     print("please enter a positive number")
 else:
@@ -6,3 +7,7 @@ else:
         print("even")
     else:
         print("odd")
+for i in range(1,n+1):
+    if i%2==0:
+        sum=sum+i
+print("the sum of even numbers is:",sum)        
