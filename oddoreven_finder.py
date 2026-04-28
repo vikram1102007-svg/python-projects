c9a37615754481a5a0b86890d9de1b8385d440de
vikram1102007 <vikram1102007@gmail.com>
2026-04-28 23:29:13 +0530

    character of odd or even file

--- oddoreven_finder.py
+++ oddoreven_finder.py
@@ -1,4 +1,5 @@
 n=int(input("enter your number:"))
+sum=0
 odd_sum=0
 even_sum=0
 if n<0:
@@ -15,3 +16,8 @@ for i in range(1,n+1):
         odd_sum+=i
 print("the sum of even numbers is:",even_sum)          
 print("the sum of odd numbers is:",odd_sum)
+
+for i in range(1,n+1):
+    sum+=i
+print("the sum of all numbers is:",sum)
+
