a1df13287c5e4a1621a47edfed27f75f65a5bb56
vikram1102007 <vikram1102007@gmail.com>
2026-04-29 21:13:15 +0530

    logic of factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n<0:
+        print("please enter a positive number")
+    elif n==0 or n==1:
+        return 1
+    else:
+        fact=1
+        for i in range(2,n+1):
+            fact*=i
+        return fact
+n=int(input("enter your number:"))
+print("the factorial of",n,"is:",factorial(n))
--- oddoreven_finder.py
+++ oddoreven_finder.py
@@ -1,3 +1,4 @@
+
 n=int(input("enter your number:"))
 sum=0
 odd_sum=0
@@ -20,4 +21,3 @@ print("the sum of odd numbers is:",odd_sum)
 for i in range(1,n+1):
     sum+=i
 print("the sum of all numbers is:",sum)
-
