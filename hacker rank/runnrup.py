9a844fd4a08e78f382a97f0937d5cb5598b3b0fa
vikram1102007 <vikram1102007@gmail.com>
2026-05-03 14:32:47 +0530

    logic of runnrup

--- hacker rank/runnrup.py
+++ hacker rank/runnrup.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    dec=sort(arr)
-    arr=list(array(arr))
+    arr=list(arr)
+    arr=list(set(arr))
+    arr=sorted(arr, reverse=True)
     print(arr[1])
     
