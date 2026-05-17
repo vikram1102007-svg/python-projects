985624638eaa7438f40dee81c5502393fb64ab96
vikram1102007 <vikram1102007@gmail.com>
2026-05-17 23:36:10 +0530

    updated code

--- hacker rank/merge alternative strings.py
+++ hacker rank/merge alternative strings.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        a=[]
-        for x,y in zip(word1,word2):
+        a = []
+
+        for x, y in zip(word1, word2):
             a.append(x)
             a.append(y)
+
         a.append(word1[len(word2):])
         a.append(word2[len(word1):])
+
         return "".join(a)
 
-        
-        
+n1 = Solution()
+n2 = Solution()
+
+print(n1.mergeAlternately("abc", "pqr"))
+print(n2.mergeAlternately("ab", "pqrs"))
