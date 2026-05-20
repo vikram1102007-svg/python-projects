bffbbec3f171fa65cfea744703051368dac80a25
vikram1102007 <vikram1102007@gmail.com>
2026-05-20 11:54:27 +0530

    updated code

--- hacker rank/maxcandies.py
+++ hacker rank/maxcandies.py
@@ -1,13 +1,6 @@
 class Solution(object):
-    def maxcan(self,candies):
-        maxcan=candies[0]
-        for i in candies:
-            if maxcan<i:
-                maxcan=i
-        return maxcan
-
     def kidsWithCandies(self, candies, extraCandies):
-        maximum=self.maxcan(candies)
+        maximum=max(candies)
         m=[]
         for i in candies:
             a=i+extraCandies
@@ -16,5 +9,3 @@ class Solution(object):
             else:
                 m.append(False)
         return m
-c1=Solution()
-print(c1.kidsWithCandies([2,3,5,1,3],3))
