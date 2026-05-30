4397311288e6f5289922ed4ee04dbef662d1f66e
vikram1102007 <vikram1102007@gmail.com>
2026-05-30 17:44:43 +0530

    updated code

---
+++ hacker rank/flower.py
@@ -0,0 +1,3 @@
+import NumPy as np
+arr=np.array([1,2,3])
+print(arr[1,2])
--- hacker rank/learn.py
+++ hacker rank/learn.py
@@ -1,4 +1,11 @@
-try:
-    a=10/0
-except ZeroDivisionError:
-    print("there is a zero division error in the code")
+import csv
+with open("sample.txt","w",newline="")as f:
+    writer=csv.writer(f)
+    writer.writerow(["name","age","city"])
+    writer.writerow(["Alice",30,"New York"])
+    writer.writerow(["Bob",25,"Los Angeles"])
+    
+    with open("sample.txt","r")as f:
+        reader=csv.reader(f)
+        for row in reader:
+            print(row)
